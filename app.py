de30e39de9864b2646948805054c0252f1656134
Vinay Vishwakarma <vinay.vishwakarma@accenture.com>
2020-06-28 13:31:06 +0100

    Adding post method

--- app.py
+++ app.py
@@ -1,28 +1,47 @@
 # This application is using color from https://colorhunt.co/
 
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index.html', methods=['GET', 'POST'])
 def index():
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid='
-    city_name = 'Lucknow'
-    response = requests.get(url.format(city_name)).json()
+    if request.method == 'POST' :
+        city_name = request.form['city']
 
-    temperature_in_degree = "{:10.2f}".format(((response['main']['temp']) - 273.15))
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid='
+        response = requests.get(url.format(city_name)).json()
 
-    weather_of_city = {
-        'city' : city_name,
-        'temperature' : temperature_in_degree,
-        'description' : response['weather'][0]['description'],
-        'icon' : response['weather'][0]['icon'],
-    }
+        temperature_in_degree = "{:10.2f}".format(((response['main']['temp']) - 273.15))
+        weather_of_city = {
+            'city' : city_name,
+            'temperature' : temperature_in_degree,
+            'description' : response['weather'][0]['description'],
+            'icon' : response['weather'][0]['icon'],
+        }
+        print(weather_of_city) 
+        return render_template('index.html', weather = weather_of_city)
 
-    print(weather_of_city)
+    else:
 
-    return render_template('index.html', weather = weather_of_city)
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid='
+        city_name = 'Lucknow'
+        response = requests.get(url.format(city_name)).json()
+        temperature_in_degree = "{:10.2f}".format(((response['main']['temp']) - 273.15))
+        weather_of_city = {
+            'city' : city_name,
+            'temperature' : temperature_in_degree,
+            'description' : response['weather'][0]['description'],
+            'icon' : response['weather'][0]['icon'],
+        }
+
+
+
+        return render_template('index.html', weather = weather_of_city)
+        
+
 
 app.run()
--- weather_script.py
+++ weather_script.py
@@ -2,7 +2,21 @@ import requests
 
 url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid='
 
-city_name = 'Lucknow'
+city_name = 'New Delhi'
 
-res = requests.get(url.format(city_name)).json()
-print(res)
+response = requests.get(url.format(city_name)).json()
+
+
+
+weather_of_city = {
+        'city' : city_name,
+        'temperature' : response['main']['temp'],
+        'description' : response['weather'][0]['description'],
+        'icon' : response['weather'][0]['icon'],
+    }
+
+print (weather_of_city)
+
+degree_celsius = response['main']['temp'] - 273.15
+
+print("Temp in degreee celsius:{:10.2f}".format(degree_celsius))
